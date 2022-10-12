51673e95b1c7934072a2dd4017d44d079ea97ac9
walkbyfaith@163.com <walkbyfaith@163.com>
2022-10-12 09:47:02 +0800

    v15

--- src/main/java/com/lyxy/git/GitTest.java
+++ src/main/java/com/lyxy/git/GitTest.java
@@ -11,5 +11,6 @@ public class GitTest {
         System.out.println("hello git5.......");
         System.out.println("hello git6.......");
         System.out.println("hello git13.......");
+        System.out.println("hello git14.......");
     }
 }
